ad358acdbaaa93df767f867559e0e586b69ca91d
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2026-07-22 07:21:42 -0700

    Added development database config

--- server/config.lua
+++ server/config.lua
@@ -3,5 +3,11 @@ local config = require("lapis.config")
 config("development", {
   server = "nginx",
   code_cache = "off",
-  num_workers = "1"
+  num_workers = "1",
+  mysql = {
+    host = assert(os.getenv("DB_HOST"), "DB_HOST must be set"),
+    user = assert(os.getenv("DB_USER"), "DB_USER must be set"),
+    password = assert(os.getenv("DB_PASSWORD"), "DB_PASSWORD must be set"),
+    database = assert(os.getenv("DB_DATABASE"), "DB_DATABASE must be set"),
+  }
 })
